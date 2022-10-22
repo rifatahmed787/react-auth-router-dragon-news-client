489f0d34301e54e303e746e7da8dc0b008e0d4fd
Rifat <mdrifatahmed787@gmail.com>
2022-10-23 02:49:22 +0600

    footer added

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center">
-      <h1>This is footer</h1>
+      <p>
+        <small>Copyright coming soon</small>
+      </p>
     </div>
   );
 };
